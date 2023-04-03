e8c19e35bb89445aa6dcaa9e95e5d31f604be8c2
nugo128 <nugo.rostiashvili@redberry.ge>
2023-04-03 14:23:10 +0400

    chore: small markup change

--- app/Models/Quote.php
+++ app/Models/Quote.php
@@ -9,13 +9,7 @@ class Quote extends Model
 {
     use HasFactory;
 
-    public function filter()
-    {
-        $movieId=36;
-        $quotes = Quote::whereHas('movie', function ($query) use ($movieId) {
-            $query->where('id', $movieId);
-        })->get();
-    }
+
     public function movie()
     {
         return $this->belongsTo(Movie::class);
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,9 +1,9 @@
 <x-layout>
 <section class="flex flex-col gap-10 justify-center align-middle w-screen items-center">
-    <div class="w-46 h-23">
-    <img src="https://trendingpics.com/app/images/download.php?key=y85J6AEA" class="w-46 h-23"></img>
+    <div class="w-1/2">
+    <img src="{{$quote->thumbnail}}"></img>
     </div>
-    <div><p class="text-white text-4xl">{{$quote->quote}}</p></div>
+    <div><p class="text-white text-4xl">"{{$quote->quote}}"</p></div>
     <div><p class="underline text-white text-4xl mt-12">{{$movie->title}}</p></div>
 </section>
 </x-layout>
