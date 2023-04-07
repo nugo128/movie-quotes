0a10183b4319264b3544556d41104b8bf7e2eca7
nugo128 <nugo.rostiashvili@redberry.ge>
2023-04-07 11:53:20 +0400

    feat: movie-quote delete connection

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,10 +1,14 @@
 <x-layout name="{{auth()->check() ? $user->name: ''}}">
 <section class="flex flex-col gap-10 justify-center align-middle w-screen items-center">
+    @if ($movie)
     <div class="w-1/2">
     <img src="/storage/{{$quote->thumbnail}}"></img>
     <p></p>
     </div>
     <div><p class="text-white text-4xl">"{{$quote->quote}}"</p></div>
-    <div><a href="{{ route('films.index', ['id' => $movie->id]) }}" class="underline text-white text-4xl mt-12">{{$movie->title}}</a></div>
+    <div><a href="{{ route('films.index', ['id' => $movie->id]) }}" class="underline text-white text-4xl mt-12">{{$movie->title}}</a></div> 
+    @else
+    <h2 class="text-2xl text-white">there are no movies added yet! you can <a href="/login" class="underline">Log In</a> and add your desired movies and quotes</h2>       
+    @endif
 </section>
 </x-layout>
--- resources/views/manage/movies/index.blade.php
+++ resources/views/manage/movies/index.blade.php
@@ -1,6 +1,6 @@
 <x-layout name='{{ $user->name }}'>
 
-<div class="flex flex-col w-full items-center mt-7">
+<div class="flex flex-col w-full items-center mt-7 mb-10">
     <p class="text-white text-2xl mb-4">Manage movies</p>
     <div id="div1" class="flex-grow-1 bg-white p-8 rounded-md">
         <div class="flex items-center justify-between pb-6">
@@ -61,7 +61,7 @@
                                 <tbody>
                                     @foreach ($quote as $quotes)
                                     <tr>
-                                        <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                        <td class="px-10 py-5 border-b border-gray-200 bg-white text-sm">
                                             <div class="flex items-center">
                                                 <div class="ml-3">
                                                     <p class="text-gray-900 whitespace-no-wrap">
@@ -70,6 +70,11 @@
                                                         </a>
                                                     </p>
                                                 </div>
+                                            </div>
+                                        </td>
+                                        <td class="px-10 py-2">
+                                            <div class="w-44">
+                                                <img src="/storage/{{$quotes->thumbnail}}" alt="">
                                             </div>
                                         </td>
 
