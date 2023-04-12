2098d641f69ef21a78de70b031c10405ab3d1745
nugo128 <nugo.rostiashvili@redberry.ge>
2023-04-12 10:45:01 +0400

    chore: add text when no movies available

--- lang/en/nav.php
+++ lang/en/nav.php
@@ -2,5 +2,6 @@
 return [
     'admin' => 'admin panel',
     'logo_out'=>'Log Out',
-    'welcome'=>'Welcome :name'
+    'welcome'=>'Welcome :name',
+    'no_movies'=>'there are no movies, log in to add some' 
 ];
