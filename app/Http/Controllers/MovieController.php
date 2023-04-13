8f2f6c7fc1a67db2d2df2d05b63a28be1ca60fbe
nugo128 <nugo.rostiashvili@redberry.ge>
2023-04-13 22:32:29 +0400

    refactor: use Movie::with

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -8,19 +8,12 @@ use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
-
-    public function show(Movie $movie,$id)
+    public function show(Movie $movie, $id)
     {
-        $movie = Movie::findOrFail($id);
+        $movie = Movie::with('quote')->findOrFail($id);
         $quote = $movie->quote;
-
-
         $user = auth()->user();
 
-
-        $quote = Quote::where('movie_id', $movie->id)->get();
-
-        $user = auth()->user();
         return view('films.index', compact('movie', 'quote', 'user'));
     }
 }
