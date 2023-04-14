bc5c36984a20794d93860320eb4de0c8a73c532f
nugo128 <nugo.rostiashvili@redberry.ge>
2023-04-14 23:55:36 +0400

    fix: get random quote instead of movie

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,7 +11,6 @@ class HomeController extends Controller
     public function index()
     {
         $quote = Quote::with('movie')->inRandomOrder()->first();
-        $movie = $quote ? $quote->movie : null;
-        return view('home.index', compact('movie', 'quote'));
+        return view('home.index', compact('quote'));
     }
 }
